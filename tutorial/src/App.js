e0748c5d77be16d8cea1d905fd19b1b29e506ed6
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-10-18 15:18:55 +0630

    #add: Typography

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -7,13 +7,16 @@ import SaveIcon from "@mui/icons-material/Save";
 import DeleteIcon from "@mui/icons-material/Delete";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
-import { TextField } from "@mui/material";
+import { TextField, Typography } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { green, purple } from "@mui/material/colors";
 
 import "./App.css";
 
 const theme = createTheme({
+    typography: {
+        fontFamily: "Raleway, Arial",
+    },
     palette: {
         primary: {
             main: green[300],
@@ -27,42 +30,46 @@ function App() {
     const [checked, setChecked] = React.useState(false);
     return (
         <ThemeProvider theme={theme}>
-            <div className="App">
-                <div>
-                    <ButtonGroup size="large" variant="contained">
-                        <Button color="primary" startIcon={<SaveIcon />}>
-                            Save
-                        </Button>
-                        <Button color="error" startIcon={<DeleteIcon />}>
-                            Delete
-                        </Button>
-                    </ButtonGroup>
-                </div>
-                <div>
-                    <FormControlLabel
-                        control={
-                            <Checkbox
-                                checked={checked}
-                                icon={<BookmarkBorderIcon />}
-                                checkedIcon={<BookmarkIcon />}
-                                onChange={(e) => setChecked(e.target.checked)}
-                                inputProps={{
-                                    "aria-label": "Checkbox",
-                                }}
-                            />
-                        }
-                        label="Bookmark"
-                    />
-                </div>
-                <div>
-                    <TextField
-                        variant="outlined"
-                        label="Email"
-                        placeholder="johndoe@example.com"
-                        color="secondary"
-                        type="email"
-                    />
-                </div>
+            <div>
+                <ButtonGroup size="large" variant="contained">
+                    <Button color="primary" startIcon={<SaveIcon />}>
+                        Save
+                    </Button>
+                    <Button color="error" startIcon={<DeleteIcon />}>
+                        Delete
+                    </Button>
+                </ButtonGroup>
+            </div>
+            <div>
+                <FormControlLabel
+                    control={
+                        <Checkbox
+                            checked={checked}
+                            icon={<BookmarkBorderIcon />}
+                            checkedIcon={<BookmarkIcon />}
+                            onChange={(e) => setChecked(e.target.checked)}
+                            inputProps={{
+                                "aria-label": "Checkbox",
+                            }}
+                        />
+                    }
+                    label="Bookmark"
+                />
+            </div>
+            <div>
+                <TextField
+                    variant="outlined"
+                    label="Email"
+                    placeholder="johndoe@example.com"
+                    color="secondary"
+                    type="email"
+                />
+            </div>
+            <div>
+                <Typography variant="h3">This is h3</Typography>
+                <Typography variant="h3" component="div">
+                    This is h3
+                </Typography>
             </div>
         </ThemeProvider>
     );
