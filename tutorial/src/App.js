312ad0eede2070fae2b365421178c19fd7e4017d
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-10-18 14:49:11 +0630

    #add: Custom Theme

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -8,49 +8,63 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
 import { TextField } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { green, purple } from "@mui/material/colors";
 
 import "./App.css";
 
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: green[300],
+        },
+        error: {
+            main: purple[300],
+        },
+    },
+});
 function App() {
     const [checked, setChecked] = React.useState(false);
     return (
-        <div className="App">
-            <div>
-                <ButtonGroup size="large" variant="contained">
-                    <Button color="primary" startIcon={<SaveIcon />}>
-                        Save
-                    </Button>
-                    <Button color="error" startIcon={<DeleteIcon />}>
-                        Delete
-                    </Button>
-                </ButtonGroup>
+        <ThemeProvider theme={theme}>
+            <div className="App">
+                <div>
+                    <ButtonGroup size="large" variant="contained">
+                        <Button color="primary" startIcon={<SaveIcon />}>
+                            Save
+                        </Button>
+                        <Button color="error" startIcon={<DeleteIcon />}>
+                            Delete
+                        </Button>
+                    </ButtonGroup>
+                </div>
+                <div>
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                checked={checked}
+                                icon={<BookmarkBorderIcon />}
+                                checkedIcon={<BookmarkIcon />}
+                                onChange={(e) => setChecked(e.target.checked)}
+                                inputProps={{
+                                    "aria-label": "Checkbox",
+                                }}
+                            />
+                        }
+                        label="Bookmark"
+                    />
+                </div>
+                <div>
+                    <TextField
+                        variant="outlined"
+                        label="Email"
+                        placeholder="johndoe@example.com"
+                        color="secondary"
+                        type="email"
+                    />
+                </div>
             </div>
-            <div>
-                <FormControlLabel
-                    control={
-                        <Checkbox
-                            checked={checked}
-                            icon={<BookmarkBorderIcon />}
-                            checkedIcon={<BookmarkIcon />}
-                            onChange={(e) => setChecked(e.target.checked)}
-                            inputProps={{
-                                "aria-label": "Checkbox",
-                            }}
-                        />
-                    }
-                    label="Bookmark"
-                />
-            </div>
-            <div>
-                <TextField
-                    variant="outlined"
-                    label="Email"
-                    placeholder="johndoe@example.com"
-                    color="secondary"
-                    type="email"
-                />
-            </div>
-        </div>
+        </ThemeProvider>
     );
 }
 
