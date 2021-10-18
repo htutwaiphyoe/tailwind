cd411b271d09b7c7d75ce697820334fc67f145ab
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-10-18 17:19:13 +0630

    #add: Container & Grid

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -7,7 +7,7 @@ import SaveIcon from "@mui/icons-material/Save";
 import DeleteIcon from "@mui/icons-material/Delete";
 import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 import BookmarkIcon from "@mui/icons-material/Bookmark";
-import { TextField, Typography } from "@mui/material";
+import { TextField, Typography, Container, Grid, Paper } from "@mui/material";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { green, purple } from "@mui/material/colors";
 
@@ -30,47 +30,61 @@ function App() {
     const [checked, setChecked] = React.useState(false);
     return (
         <ThemeProvider theme={theme}>
-            <div>
-                <ButtonGroup size="large" variant="contained">
-                    <Button color="primary" startIcon={<SaveIcon />}>
-                        Save
-                    </Button>
-                    <Button color="error" startIcon={<DeleteIcon />}>
-                        Delete
-                    </Button>
-                </ButtonGroup>
-            </div>
-            <div>
-                <FormControlLabel
-                    control={
-                        <Checkbox
-                            checked={checked}
-                            icon={<BookmarkBorderIcon />}
-                            checkedIcon={<BookmarkIcon />}
-                            onChange={(e) => setChecked(e.target.checked)}
-                            inputProps={{
-                                "aria-label": "Checkbox",
-                            }}
-                        />
-                    }
-                    label="Bookmark"
-                />
-            </div>
-            <div>
-                <TextField
-                    variant="outlined"
-                    label="Email"
-                    placeholder="johndoe@example.com"
-                    color="secondary"
-                    type="email"
-                />
-            </div>
-            <div>
-                <Typography variant="h3">This is h3</Typography>
-                <Typography variant="h3" component="div">
-                    This is h3
-                </Typography>
-            </div>
+            <Container maxWidth="sm">
+                <div>
+                    <ButtonGroup size="large" variant="contained">
+                        <Button color="primary" startIcon={<SaveIcon />}>
+                            Save
+                        </Button>
+                        <Button color="error" startIcon={<DeleteIcon />}>
+                            Delete
+                        </Button>
+                    </ButtonGroup>
+                </div>
+                <div>
+                    <FormControlLabel
+                        control={
+                            <Checkbox
+                                checked={checked}
+                                icon={<BookmarkBorderIcon />}
+                                checkedIcon={<BookmarkIcon />}
+                                onChange={(e) => setChecked(e.target.checked)}
+                                inputProps={{
+                                    "aria-label": "Checkbox",
+                                }}
+                            />
+                        }
+                        label="Bookmark"
+                    />
+                </div>
+                <div>
+                    <TextField
+                        variant="outlined"
+                        label="Email"
+                        placeholder="johndoe@example.com"
+                        color="secondary"
+                        type="email"
+                    />
+                </div>
+                <div>
+                    <Typography variant="h3">This is h3</Typography>
+                    <Typography variant="h3" component="div">
+                        This is h3
+                    </Typography>
+                </div>
+
+                <Grid container spacing="10">
+                    <Grid item>
+                        <Paper style={{ width: 50, height: 75 }} />
+                    </Grid>
+                    <Grid item>
+                        <Paper style={{ width: 50, height: 75 }} />
+                    </Grid>
+                    <Grid item>
+                        <Paper style={{ width: 50, height: 75 }} />
+                    </Grid>
+                </Grid>
+            </Container>
         </ThemeProvider>
     );
 }
