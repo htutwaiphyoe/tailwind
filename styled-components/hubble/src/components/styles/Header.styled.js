9578bdb05691ec44a58f11ff0f023fd889b94e13
htutwaiphyoe <htutwaiphyoe@gmail.com>
2021-10-19 17:11:32 +0630

    #add: nesting

--- styled-components/hubble/src/components/styles/Header.styled.js
+++ styled-components/hubble/src/components/styles/Header.styled.js
@@ -3,4 +3,12 @@ import styled from "styled-components";
 export const StyledHeader = styled.header`
     background-color: #ebfbff;
     padding: 40px 0;
+
+    & h1 {
+        color: red;
+    }
+
+    &:hover {
+        background-color: #111;
+    }
 `;
